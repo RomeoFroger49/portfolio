70d6703443a451632dffe259240dbac9644195a3
Romeo Froger <romeo.froger@gmail.com>
2023-12-07 00:14:28 +0100

    maj package

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio",
   plugins: [react()],
-  build: {
-    outDir: "build", // Change this to your desired output directory
+  esbuild: {
+    jsxFactory: "h", // or your preferred JSX factory function
+    jsxInject: `import 'react'`, // or the path to your React runtime
   },
 });
