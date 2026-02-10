f46f964df47cb73ed949803bc8b2ac8627bb2e50
RomeoFroger49 <romeo.froger@gmail.com>
2026-02-11 00:29:18 +0100

    fix: text section update

--- src/components/SectionTexte.jsx
+++ src/components/SectionTexte.jsx
@@ -54,9 +54,12 @@ export default function SectionTexte() {
         className="xl:text-4xl text-lg font-satoshiMedium  text-[rgb(209,209,200)] sm:leading-[5rem] leading-[3rem]"
         ref={sectionRef}
       >
-        <i id="i-section">Roméo Froger</i>, a third-year information sciences
-        student, passionately explores <i id="i-section">Web development</i>.
-        Combining creativity and expertise.
+        <i id="i-section">Roméo Froger</i>, a first-year computer engineer
+        student, I design and build modern, efficient web applications — from
+        intuitive UI to robust back-end systems. Driven by curiosity, I’m also
+        exploring <i id="i-section">DevOps</i> and{" "}
+        <i id="i-section">applied AI</i> to create scalable, real-world
+        solutions.
       </p>
     </section>
   );
